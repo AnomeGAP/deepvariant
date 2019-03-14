8c3492597cefeb224522e19f08d756c09246748e
A-Tsai <chungtsai@gmail.com>
2019-03-14 15:01:28 +0800

    rollback

--- deepvariant/call_variants.py
+++ deepvariant/call_variants.py
@@ -327,7 +327,7 @@ def call_variants(examples_filename,
                    gpu_options=tf.GPUOptions(
                        per_process_gpu_memory_fraction=float(FLAGS.per_process_gpu_memory_fraction / 100))) \
       if FLAGS.enable_configurable_gpu else tf.ConfigProto(device_count=device_count)
-  with tf.Session(config=config) as sess:
+  with tf.Session(config=tf.ConfigProto(device_count=device_count)) as sess:
     sess.run(init_op)
     if execution_hardware == 'accelerator':
       if not any(dev.device_type != 'CPU' for dev in sess.list_devices()):
